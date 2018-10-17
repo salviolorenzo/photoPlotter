b1d2cecd8daaf150ed9e7db2ad96d3b5c194cf14
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-10-17 19:11:39 -0400

    adds array of urls for testing

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 let userSearch = 'sunset';
+urlArray = [`https://api.unsplash.com/photos/Cbh93NpfWeg?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`,
+`https://api.unsplash.com/photos/pFS8jgu8-ag?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`,
+`https://api.unsplash.com/photos/dBaz0xhCkPY?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`,
+`https://api.unsplash.com/photos/wLLrbu9qqX8?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`,
+`https://api.unsplash.com/photos/x8RTvVS8DUc?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`,
+`https://api.unsplash.com/photos/i3k4BZcHa7c?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`,
+`https://api.unsplash.com/photos/HgaAmMhpsfg?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`,
+`https://api.unsplash.com/photos/wLLrbu9qqX8?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`,
+`https://api.unsplash.com/photos/viiHGcf7_yw?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`,
+`https://api.unsplash.com/photos/J-0k2rxxirg?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`
+]
 
-fetch(`https://api.unsplash.com/photos/Cbh93NpfWeg?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`)
-.then(r => r.json())
-.then(x => plotPoint(x.location));
-
+function main(){
+    fetch(urlArray[9])
+    .then(r => r.json())
+    .then(x => plotPoint(x.location));
+}
 let listofObj = [];
 locations = [];
 
@@ -15,12 +27,14 @@ function getResults(obj){
     return listofObj;
 }
 
+// function getLocation()
+
 function plotPoint(location){
     let long = location.position.longitude;
     let lat = location.position.latitude;
     console.log(long);
     console.log(lat);
-    map.src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyDX7NHyx9RpNMLF1Hbd6vwuXy_ESzR9oSE
-    &q=${lat}${long}`
+    map.src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyDX7NHyx9RpNMLF1Hbd6vwuXy_ESzR9oSE&q=${lat}${long}`;
 }
 
+main();
