7ca70944273ee1feecb49665aea8f95fe1b1bcca
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-10-18 09:29:04 -0400

    adds security

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let userSearch = 'sunset';
-urlArray = [`https://api.unsplash.com/photos/Cbh93NpfWeg?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`,
-`https://api.unsplash.com/photos/pFS8jgu8-ag?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`,
-`https://api.unsplash.com/photos/dBaz0xhCkPY?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`,
-`https://api.unsplash.com/photos/wLLrbu9qqX8?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`,
-`https://api.unsplash.com/photos/x8RTvVS8DUc?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`,
-`https://api.unsplash.com/photos/i3k4BZcHa7c?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`,
-`https://api.unsplash.com/photos/HgaAmMhpsfg?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`,
-`https://api.unsplash.com/photos/wLLrbu9qqX8?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`,
-`https://api.unsplash.com/photos/viiHGcf7_yw?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`,
-`https://api.unsplash.com/photos/J-0k2rxxirg?client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`
+urlArray = [`https://api.unsplash.com/photos/Cbh93NpfWeg?client_id=${USKey}`,
+`https://api.unsplash.com/photos/pFS8jgu8-ag?client_id=${USKey}`,
+`https://api.unsplash.com/photos/dBaz0xhCkPY?client_id=${USKey}`,
+`https://api.unsplash.com/photos/wLLrbu9qqX8?client_id=${USKey}`,
+`https://api.unsplash.com/photos/x8RTvVS8DUc?client_id=${USKey}`,
+`https://api.unsplash.com/photos/i3k4BZcHa7c?client_id=${USKey}`,
+`https://api.unsplash.com/photos/HgaAmMhpsfg?client_id=${USKey}`,
+`https://api.unsplash.com/photos/wLLrbu9qqX8?client_id=${USKey}`,
+`https://api.unsplash.com/photos/viiHGcf7_yw?client_id=${USKey}`,
+`https://api.unsplash.com/photos/J-0k2rxxirg?client_id=${USKey}`
 ]
 
 function main(){
@@ -34,7 +34,7 @@ function plotPoint(location){
     let lat = location.position.latitude;
     console.log(long);
     console.log(lat);
-    map.src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyDX7NHyx9RpNMLF1Hbd6vwuXy_ESzR9oSE&q=${lat}${long}`;
+    map.src = `https://www.google.com/maps/embed/v1/place?key=${GoKey}&q=${lat}${long}`;
 }
 
 main();
